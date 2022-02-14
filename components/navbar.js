59434b4c124d5d5120e2da3e52fea292febf323b
Samuel Adjei <info.samadjei@gmail.com>
2022-02-14 22:45:26 +0000

    added assets to public folder

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
 	return (
-		<div>
-			<p>This is a navbar</p>
-		</div>
+		<nav className="nav">
+			<div className="container">
+				<div className="logo">
+					<Link href="/">
+						<a>
+							<Image src="/assets/shared/desktop/logo.svg" alt="Picture of the author" width={143} height={25} />
+						</a>
+					</Link>
+				</div>
+				<ul className="nav__links">
+					<li className="nav__links-items">
+						<Link href="/">
+							<a>Home</a>
+						</Link>
+					</li>
+					<li className="nav__links-items">
+						<Link href="/about">
+							<a>About Us</a>
+						</Link>
+					</li>
+					<li className="nav__links-items">
+						<Link href="/blog/hello-world">
+							<a>Blog Post</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	);
 };
 
--- pages/homepage.js
+++ pages/homepage.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Homepage = () => {
 	return (
 		<main>
-			<h1 className="title">
-				Welcome to <a href="https://nextjs.org">Next.js!</a>
-			</h1>
+			<h1 className="title"></h1>
 		</main>
 	);
 };
