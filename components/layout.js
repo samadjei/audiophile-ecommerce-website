07e17bd4f66cf5b0eceda0bb723f1af92562c75f
Samuel Adjei <info.samadjei@gmail.com>
2022-02-14 22:13:31 +0000

    added design system

--- components/layout.js
+++ components/layout.js
@@ -9,8 +9,9 @@ const Layout = ({ children }) => {
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 
-				{/* <link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link href="https://fonts.googleapis.com/css2?family=Lexend+Deca&family=Space+Mono:ital,wght@0,700;1,400&display=swap" rel="stylesheet" /> */}
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap" rel="stylesheet" />
 			</Head>
 			<div>
 				<Navbar />
