5f8416f4919ff9baffc7fdfc351359c38d10ac95
Samuel Adjei <info.samadjei@gmail.com>
2022-03-08 12:59:40 +0000

    added headphones page

--- pages/Headphones.js
+++ pages/Headphones.js
@@ -5,50 +5,67 @@ import Button from '../components/Button';
 import Categories from '../components/Categories';
 import AudioGear from '../components/Home/AudioGear';
 
+const data = [
+	{
+		overline: 'New Product',
+		title: 'XX99 Mark II Headphones',
+		content: 'The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.',
+		desktopImage: '/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg',
+		tabletImage: '/assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg',
+		mobileImage: '/assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg',
+		alt: 'Headphones',
+	},
+	{
+		title: 'XX99 Mark I Headphones',
+		content: 'As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.',
+		desktopImage: '/assets/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg',
+		tabletImage: '/assets/product-xx99-mark-one-headphones/tablet/image-category-page-preview.jpg',
+		mobileImage: '/assets/product-xx99-mark-one-headphones/mobile/image-category-page-preview.jpg',
+		alt: 'Headphones',
+	},
+	{
+		title: 'XX59 Headphones',
+		content: 'Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.',
+		desktopImage: '/assets/product-xx59-headphones/desktop/image-category-page-preview.jpg',
+		tabletImage: '/assets/product-xx59-headphones/tablet/image-category-page-preview.jpg',
+		mobileImage: '/assets/product-xx59-headphones/mobile/image-category-page-preview.jpg',
+		alt: 'Headphones',
+	},
+];
+
 const Headphones = () => {
 	return (
 		<div className="headphones">
 			<PageHeaders header="Headphones" />
 			<div className="container">
-				<div className="headphones__grid">
-					<Image src="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg" alt="Audiophile Logo" width={540} height={560} />
-					<div className="headphones__content">
-						<span className="overline">New Product</span>
-						<h2 className="page--title">XX99 Mark II Headphones</h2>
-						<p className="page--text">The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
-						<div>
-							<Link href="/Headphones">
-								<Button children="See Product" className="btn" buttonStyle="btn--primary" buttonSize="btn--desktop" />
-							</Link>
+				{data.map((card, index) => (
+					<div key={index} className="page__grid">
+						<div className="page__image-desktop">
+							<Image src={card.desktopImage} alt={card.alt} width={540} height={560} />
+						</div>
+						<div className="page__image-tablet">
+							<Image src={card.tabletImage} alt={card.alt} width={689} height={352} />
+						</div>
+						<div className="page__image-mobile">
+							<Image src={card.mobileImage} alt={card.alt} width={327} height={352} />
+						</div>
+						<div className="headphones__content">
+							<span className="overline">{card.overline}</span>
+							<h2 className="page--title">{card.title}</h2>
+							<p className="page--text">{card.content}</p>
+							<div>
+								<Link href="/Headphones">
+									<Button children="See Product" className="btn" buttonStyle="btn--primary" buttonSize="btn--desktop" />
+								</Link>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className="headphones__grid">
-					<div className="headphones__content">
-						<h2 className="page--title">XX99 Mark I Headphones</h2>
-						<p className="page--text">As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.</p>
-						<div>
-							<Link href="/Headphones">
-								<Button children="See Product" className="btn" buttonStyle="btn--primary" buttonSize="btn--desktop" />
-							</Link>
-						</div>
-					</div>
-					<Image src="/assets/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg" alt="Audiophile Logo" width={540} height={560} />
-				</div>
-				<div className="headphones__grid">
-					<Image src="/assets/product-xx59-headphones/desktop/image-category-page-preview.jpg" alt="Audiophile Logo" width={540} height={560} />
-					<div className="headphones__content">
-						<h2 className="page--title">XX59 Headphones</h2>
-						<p className="page--text">Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.</p>
-						<div>
-							<Link href="/Headphones">
-								<Button children="See Product" className="btn" buttonStyle="btn--primary" buttonSize="btn--desktop" />
-							</Link>
-						</div>
-					</div>
-				</div>
+				))}
+
 				<Categories />
-				<AudioGear />
+				<div className="headphones__gear">
+					<AudioGear />
+				</div>
 			</div>
 		</div>
 	);
