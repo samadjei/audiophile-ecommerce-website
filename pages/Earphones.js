805a2c98cd1e3ab0eedced527f990cb6cd2d46bb
Samuel Adjei <info.samadjei@gmail.com>
2022-03-08 17:22:43 +0000

    added earphones page

--- pages/Earphones.js
+++ pages/Earphones.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import PageHeaders from '../components/PageHeaders';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '../components/Button';
+import Categories from '../components/Categories';
+import AudioGear from '../components/Home/AudioGear';
+
+const data = [
+	{
+		overline: 'New Product',
+		title: 'YX1 wireless earphones',
+		content: 'Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.',
+		desktopImage: '/assets/product-yx1-earphones/desktop/image-category-page-preview.jpg',
+		tabletImage: '/assets/product-yx1-earphones/tablet/image-category-page-preview.jpg',
+		mobileImage: '/assets/product-yx1-earphones/mobile/image-category-page-preview.jpg',
+		alt: 'YX1 wireless earphones',
+	},
+];
 
 const Earphones = () => {
 	return (
-		<div>Earphones</div>
-	)
-}
+		<div className="earphones">
+			<PageHeaders header="Earphones" />
+			<div className="container">
+				{data.map((card, index) => (
+					<div key={index} className="page__grid">
+						<div className="page__image-desktop">
+							<Image src={card.desktopImage} alt={card.alt} width={540} height={560} />
+						</div>
+						<div className="page__image-tablet">
+							<Image src={card.tabletImage} alt={card.alt} width={689} height={352} />
+						</div>
+						<div className="page__image-mobile">
+							<Image src={card.mobileImage} alt={card.alt} width={327} height={352} />
+						</div>
+						<div className="page__content">
+							<span className="overline">{card.overline}</span>
+							<h2 className="page--title">{card.title}</h2>
+							<p className="page--text">{card.content}</p>
+							<div>
+								<Link href="/Headphones">
+									<Button children="See Product" className="btn" buttonStyle="btn--primary" buttonSize="btn--desktop" />
+								</Link>
+							</div>
+						</div>
+					</div>
+				))}
 
-export default Earphones
+				<Categories />
+				<div className="page__gear">
+					<AudioGear />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Earphones;
